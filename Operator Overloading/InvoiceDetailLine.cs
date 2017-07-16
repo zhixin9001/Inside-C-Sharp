1b66badb11e3c1a313d65820925baed398ecf16a
zhixin9001 <zhixin9001@126.com>
2017-07-17 06:43:54 +0800

    add

---
+++ Operator Overloading/InvoiceDetailLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading
+{
+  public class InvoiceDetailLine
+  {
+    double lineTotal;
+
+    public double LineTotal { get => lineTotal; }
+
+    public InvoiceDetailLine(double lineTotal)
+    {
+      this.lineTotal = lineTotal;
+    }
+  }
+}
