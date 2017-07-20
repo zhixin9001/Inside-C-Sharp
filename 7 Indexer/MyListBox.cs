c46ed6b7fc9e7e2dd8823f4a65bb33c57d9cb152
zhixin9001 <zhixin9001@126.com>
2017-07-20 18:58:04 +0800

    indexer

---
+++ 7 Indexer/MyListBox.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Indexer
+{
+  class MyListBox
+  {
+    protected ArrayList data = new ArrayList();
+
+    public object this[int idx]
+    {
+      get
+      {
+        if(idx>-1&&idx<data.Count)
+        {
+          return (data[idx]);
+        }else
+        {
+          return null;
+        }
+      }
+      set
+      {
+        if(idx>-1&&idx<data.Count)
+        {
+          data[idx] = value;
+        }
+        else  if(idx==data.Count)
+        {
+          data.Add(value);
+        }
+        else
+        {
+
+        }
+      }
+    }
+  }
+}
---
+++ 7 Indexer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Indexer
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      #region MyRegion
+      MyListBox lbx = new MyListBox();
+      lbx[0] = "foo";
+      lbx[1] = "far";
+      lbx[2] = "bar";
+      Console.WriteLine("{0} {1} {2}",lbx[0],lbx[1],lbx[2]);
+      #endregion
+
+      Console.ReadKey();
+    }
+  }
+}
