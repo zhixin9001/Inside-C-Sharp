699cbc171d6bca1cb2134de7f2dcfe10761d11af
zhixin9001 <zhixin9001@126.com>
2017-07-25 07:47:42 +0800

    unmanaged code 2

---
+++ New Egg/17 Parameters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Egg
+{
+  class Parameters
+  {
+    public void TestParams(params int[] value)
+    {
+      Console.WriteLine("Using a params parameters!");
+    }
+    public void TestParams(int x, int y)
+    {
+      Console.WriteLine("Using two int parameters!");
+    }
+  }
+}
