50220e10c713c61fe86442b24cffaf6df62aa751
zhixin9001 <zhixin9001@126.com>
2017-07-28 09:23:11 +0800

    1

---
+++ 18 Assembly/Module1Server.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Assembly
+{
+  public class Module1Server
+  {
+  }
+  class Module1ClientApp
+  {
+    public static void Main()
+    {
+      Assembly DllAssembly = Assembly.GetAssembly(typeof(Module1Server));
+      Console.WriteLine("Module1Server.dll Assembly Information");
+      Console.WriteLine("\t"+DllAssembly);
+
+      Process p = Process.GetCurrentProcess();
+      string AssemblyName = p.ProcessName + ".exe";
+      Assembly ThisAssembly = Assembly.LoadFrom(AssemblyName);
+      Console.WriteLine("Module1ClientApp.dll Assembly Information");
+      Console.WriteLine("\t" + ThisAssembly);
+    }
+  }
+}
